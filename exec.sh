8aa5d061309bed2c400ea1e9f1d6256c61804036
Trevor Kimenye <root@chatcrm.(none)>
2014-02-17 22:04:28 +0300

    added log

--- exec.sh
+++ exec.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export URL="http://localhost:8080"
+export SQLALCHEMY_DATABASE_URI="mysql+mysqldb://rails:wxFKW6Fz4B@localhost/rails"
 source /home/wassup/venv/bin/activate & cd /home/wassup & python /home/wassup/server.py
