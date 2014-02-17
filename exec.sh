f96ec94633691b975101ebb649fead550e237796
Trevor Kimenye <root@chatcrm.(none)>
2014-02-17 22:19:28 +0300

    modified exec.sh

--- exec.sh
+++ exec.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 export URL="http://localhost:8080"
 export SQLALCHEMY_DATABASE_URI="mysql+mysqldb://rails:wxFKW6Fz4B@localhost/rails"
-source /home/wassup/venv/bin/activate & cd /home/wassup & python /home/wassup/server.py
+
+cd /home/wassup
+source venv/bin/activate
+python /home/wassup/server.py
